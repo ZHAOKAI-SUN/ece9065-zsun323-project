1ffe9f65478a22bccac2701042771dfa188c1f12
ZHAOKAI-SUN <55410834+ZHAOKAI-SUN@users.noreply.github.com>
2019-12-05 16:26:18 -0500

    Restore angular to the previous version
    Encountered an unsolvable bug.
    Restore angular to the previous version.

--- APP/src/app/app-routing.module.ts
+++ APP/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 // Every time add a "component", need to add "import" at here first
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 // After adding "import", add "routes" here
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
