761ce61ad930104451d4621c27887e847dd22c0e
ZHAOKAI-SUN <55410834+ZHAOKAI-SUN@users.noreply.github.com>
2019-12-09 20:29:21 -0500

    Finish: Link song to index.router
    Link song to index.router

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-const ctrlUser = require('../controllers/user.controller');
-
 const jwtHelper = require('../config/jwtHelper');
 
+
+////// 1 : Linked to the controller (Each function has its own controller)
+const ctrlUser = require('../controllers/user.controller');
+const song_controller = require('../controllers/song.controller');
+
+
+////// 2 : Add router path
+
+//// User part
 router.post('/register', ctrlUser.register);
 router.post('/authenticate', ctrlUser.authenticate);
 router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile); // private route !!! add: verifyJwtToken !!!!!
 router.get('/activate/:token', ctrlUser.activateUser); // Activate email
 router.post('/resend', ctrlUser.resend);
 
+//// Sony part
+// TEST
+router.get('/testSong', song_controller.test);
+// CREATE
+router.post('/createSong', song_controller.song_create);
+
+
+
+
+////// Just keep it
 module.exports = router;
