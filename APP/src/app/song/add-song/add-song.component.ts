c871bf2cee8b4eaf565aa9af5ddee2dd207f9b84
ZHAOKAI-SUN <55410834+ZHAOKAI-SUN@users.noreply.github.com>
2019-12-15 15:53:52 -0500

    BUG FIX : Add comments directly after creating a song, NOR and AR have no value problem
    BUG FIX : Add comments directly after creating a song, NOR and AR have no value problem

--- APP/src/app/song/add-song/add-song.component.ts
+++ APP/src/app/song/add-song/add-song.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from "@angular/router";
 
 // Step 4: add service to component
 import { SongService } from '../../shared/song.service';
@@ -17,8 +18,10 @@ export class AddSongComponent implements OnInit {
 
   showSucessMessage: boolean;
   serverErrorMessages: string;
+  newsong: any;
+  newjson: any;
 
-  constructor(private songService: SongService, private appComponent: AppComponent) { }
+  constructor(private songService: SongService, private appComponent: AppComponent, private router : Router) { }
 
   ngOnInit() {
   }
@@ -30,8 +33,19 @@ export class AddSongComponent implements OnInit {
     this.songService.postSong(form.value).subscribe(
       res => { // function 1
         this.showSucessMessage = true;
-        setTimeout(() => this.showSucessMessage = false, 4000);
+        setTimeout(() => this.showSucessMessage = false, 20000);
         this.resetForm(form);
+        this.newsong = res; // Create global variable "newsong" to fill in comments directly
+        this.newjson = {
+          "ID"  : this.newsong._id,
+          "Title" : this.newsong.title,
+          "Nor" : 0,
+          "Ar" : "0"
+        };
+        this.appComponent.selectedsong = this.newjson;
+        this.appComponent.route = 'songs/add_song';
+        console.log(this.appComponent.selectedsong);
+        console.log(this.appComponent.route);
       },
       err => { // function 2
         if (err.status === 422) {
@@ -63,6 +77,10 @@ export class AddSongComponent implements OnInit {
     };
     form.resetForm();
     this.serverErrorMessages = '';
+  }
+
+  jumptoreview(){
+    this.router.navigateByUrl('/songs/add_review');
   }
 
 }
