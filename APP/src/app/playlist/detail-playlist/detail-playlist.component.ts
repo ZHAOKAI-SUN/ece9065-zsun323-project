e7287702a30003c38254736ac9e15c8de37eb524
ZHAOKAI-SUN <55410834+ZHAOKAI-SUN@users.noreply.github.com>
2019-12-15 22:13:29 -0500

    UPDATE : Send current user and current playlist to the playlist info
    UPDATE : Send current user and current playlist to the playlist info

--- APP/src/app/playlist/detail-playlist/detail-playlist.component.ts
+++ APP/src/app/playlist/detail-playlist/detail-playlist.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SongService } from '../../shared/song.service';
+import { AppComponent } from '../../app.component';
+import { Router } from "@angular/router"; // Use for jump
+import { UserService } from '../../shared/user.service';
+import { PlaylistService } from 'src/app/shared/playlist.service';
 
 @Component({
   selector: 'app-detail-playlist',
   templateUrl: './detail-playlist.component.html',
-  styleUrls: ['./detail-playlist.component.scss']
+  styleUrls: ['./detail-playlist.component.scss'],
+  // Step 5: add providers
+  providers : [SongService, UserService, PlaylistService]
 })
 export class DetailPlaylistComponent implements OnInit {
 
-  constructor() { }
+  sbuser: any;
+  sbplaylist: any;
+
+  constructor(private songService: SongService, private userService: UserService, private playlistService: PlaylistService, private appComponent: AppComponent, private router : Router) { }
 
   ngOnInit() {
+    // get current user and current playlist
+    this.sbuser = this.appComponent.owner;
+    this.sbplaylist = this.appComponent.selectedplaylist;
+    console.log(this.sbuser);
+    console.log(this.sbplaylist);
   }
 
 }
